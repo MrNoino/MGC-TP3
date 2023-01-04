17de19cf4799cee060f0f9fe23fc88e800b6b16c
Karine Florencio <karineabf@hotmail.com>
2023-01-04 12:38:05 +0000

    movimentacao player em jogo

--- jogo/game.py
+++ jogo/game.py
@@ -1,6 +1,21 @@
 import pygame, sys
 from pygame.locals import *
 import random, time
+from player import Player
+from entity import Entity
+
+value = 0
+image_zumbiF = [pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (1).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (2).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (3).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (4).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (5).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (6).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (7).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (8).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (9).png"), (90,100)),True,False),
+                pygame.transform.flip(pygame.transform.smoothscale(pygame.image.load("png/female/Walk (10).png"),(90,100)),True,False)]
+
 
 DISPLAY = (800,500) #pygame.display.get_desktop_sizes
 
@@ -26,7 +41,7 @@ pygame.init()
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 
-background = pygame.image.load("Grass_Sample.png")
+background = pygame.image.load("Grass_Sample2.png")
 
 
 #Create a white screen
@@ -35,18 +50,28 @@ DISPLAYSURF = pygame.display.set_mode(DISPLAY)
 
 pygame.display.set_caption("Zgame")
 
+P1 = Player("P1", image_zumbiF[0],(int(DISPLAY[0]/2), int(DISPLAY[1]/2)))
+
 InitGraphics(DISPLAYSURF)
 #pygame.mixer.Sound('background.wav').play(loops=-1)
 
 #Game Loop
 while True:
+
+
     # DISPLAYSURF.fill(WHITE)
     #loops through map to set background
     for y in range(5):
         for x in range(5):
-            location = (x*704, y*320)
+            location = (x*658, y*369)
             DISPLAYSURF.blit(background, location)
     DISPLAYSURF.blit(font_small.render("Pontuação", True, RED), (10,10))
+    P1.move(DISPLAY)
+
+
+    P1.draw(DISPLAYSURF)
+
+
     #Cycles through all events occuring
     for event in pygame.event.get():
 
@@ -55,3 +80,4 @@ while True:
             sys.exit()
 
     pygame.display.flip()
+    value += 1
--- jogo/player.py
+++ jogo/player.py
@@ -6,11 +6,11 @@ class Player(entity.Entity):
 
     __username = None
 
-    def __init__(self, username, imagePath, position):
+    def __init__(self, username, image, position):
 
         self.__username = username
 
-        super.__init__(imagePath, position)
+        super().__init__(image, position)
 
     def getUsername(self):
 
@@ -24,26 +24,26 @@ class Player(entity.Entity):
 
         __pressed_keys = pygame.key.get_pressed()
 
-        if self.rect.left > 0:
+        if self._rect.left > 0:
 
             if __pressed_keys[K_LEFT] or __pressed_keys[K_a]:
 
                 super().move((-5, 0))
 
-        if self.rect.right < windowSize[0]: 
+        if self._rect.right < windowSize[0]:
 
             if __pressed_keys[K_RIGHT] or __pressed_keys[K_d]:
 
                 super().move((5, 0))
 
-        if self.rect.Top > 0: 
+        if self._rect.top > 0:
 
             if __pressed_keys[K_UP] or __pressed_keys[K_w]:
 
-                super().move(0, 5)
+                super().move((0, -5))
 
-        if self.rect.bottom > windowSize[1]: 
+        if self._rect.bottom < windowSize[1]:
 
             if __pressed_keys[K_DOWN] or __pressed_keys[K_s]:
 
-                super().move(0, -5)
+                super().move((0, 5))
