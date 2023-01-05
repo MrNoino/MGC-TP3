284b1f1b9cf0c499647a0c1f10539cb2eb79fd17
MrNoino <nuno.miguelsl25@gmail.com>
2023-01-05 20:56:05 +0000

    Added logs for exceptions and added some features to the server

--- servidor/app.py
+++ servidor/app.py
@@ -1,3 +1,22 @@
 from server import Server
+from _thread import *
 
-server = Server()
+def main():
+
+	server = Server()
+
+	while True:
+		
+		if server.getConnections() < 2:
+
+			host, addr = server.getSocket().accept()
+			print("[CONNECTION] Conectado a:", addr)
+
+			server.incrementConnections()
+			start_new_thread(server.setupClient, (host,))
+			server.incrementPlayerID()
+
+	print("[SERVER] Servidor desligado.")
+
+if __name__ == '__main__':
+    main()
--- servidor/server.py
+++ servidor/server.py
@@ -1,9 +1,15 @@
 import socket
 from _thread import *
+import pickle
+import utils
 
 class Server:
 
     def __init__(self):
+
+        self.__players = {}
+        self.__connections = 0
+        self.__playerID = 0
 
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__server = socket.gethostbyname(socket.gethostname())
@@ -15,11 +21,88 @@ class Server:
 
         except socket.error as e:
 
-            str(e)
+            print(e)
+
+            #escreve um log com a exceção
+            utils.saveLog(e)
 
             return
 
         self.__socket.listen(2)
-        print("Servidor ligado, à espera de conexões...")
+        print("[SERVER] Servidor ligado, à espera de conexões...\n")
+
+    def getSocket(self):
+
+        return self.__socket
+
+    def incrementConnections(self):
+
+        self.__connections += 1
+
+    def decrementConnections(self):
+
+        self.__connections -= 1
+
+    def incrementPlayerID(self):
+
+        self.__playerID += 1
+
+    def getConnections(self):
+
+        return self.__connections
+
+    def setupClient(self, connection):
+
+        data = connection.recv(16)
+
+        name = data.decode("utf-8")
+
+        currentID = self.__playerID
+
+        print("[LOG]", "[" + str(currentID) + "]", name, " se conectou ao servidor.\n")
+
+        self.__players[currentID] = {'name': name}
+
+        connection.send(pickle.dumps(self.__players))
+
+        while True:
+
+            try:
+
+                data = connection.recv(32)
+
+                if not data:
+                    break
+
+                data = data.decode("utf-8")
+                print("[DATA] Recebido do cliente", "[" + str(currentID) + "]", name, ": \"" + data + "\"\n")
+
+                connection.send(pickle.dumps(self.__players))
+
+            except WindowsError as e:
+
+                #escreve um log com a exceção
+                utils.saveLog(e)
+
+                break
+
+            except Exception as e:
+
+                print(e)
+
+                #escreve um log com a exceção
+                utils.saveLog(e)
+
+                break 
+
+
+        print("[LOG]", "[" + str(currentID) + "]", name,  "se desconectou.\n")
+
+        self.decrementConnections()
+
+        del self.__players[currentID]
+        
+        connection.close()
+
 
 
